ff1bcffaf66322fba2df9276a775d138884fda9c
Jorge <lsauceda2@outlook.com>
2020-01-18 11:21:26 -0600

    Request data from PokéAPI

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 
 export class Detail extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemon: null
+        }
+    }
+
+    componentDidMount() {
+        fetch(this.props.pokemon.url)
+        .then(res => res.json())
+        .then(res => {
+            this.setState({
+                pokemon: res
+            })
+        });
+    }
+
+    getName() {
+        const pokemon = this.state.pokemon;
+        if (pokemon) {
+            for (const entry of pokemon.names) {
+                if (entry.language.name === 'en') {
+                    return entry.name;
+                }
+            }
+        }
+    }
+
+    getInformation() {
+        const pokemon = this.state.pokemon;
+        if (pokemon) {
+            for (const entry of pokemon.flavor_text_entries) {
+                if (entry.language.name === 'en') {
+                    return entry.flavor_text;
+                }
+            }
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.props.pokemon.name}
+                {this.getName()}
+                <br/>
+                {this.getInformation()}
             </div>
         );
     }
